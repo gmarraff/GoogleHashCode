225a3b19ecb4d826db9e3a8cbf56a95f739f0a6b
Denis <denismazzu@gmail.com>
2018-03-01 21:46:33 +0100

    fix

--- cars/src/main/java/com/hascode/cars/ChooseNext.java
+++ cars/src/main/java/com/hascode/cars/ChooseNext.java
@@ -5,14 +5,15 @@ import java.util.List;
 class ChooseNext {
     // maxiter massimo, lowerbound massimo e slack nullo per la soluzione più stretta possibile
     public static Integer chooseNext(
-        List<Ride> r, 
-        Car c, 
-        Integer time, 
-        Integer maxTime, 
-        Integer maxIter, 
-        Integer lowerBound, 
-        Integer slack,
-        Integer bonus) {
+        List<Ride> r, // array di Rider
+        Car c, // macchina in questione
+        Integer time, // tempo corrente
+        Integer maxTime, // tempo massimo totale
+        Integer maxIter, // numero massimo di iterazioni possibili
+        Integer lowerBound, // score minimo richiesto per uscire prima
+        Integer slack, // quanto prima fare arrivare la macchina rispetto allo start time
+        Integer bonus // bonus per arrivare in tempo
+        ) {
         if (r.size() < 1) return -1; // casi base non ho elementi in r
 
         Integer out = 0, // indice (interno a r) dell'elemento scelto
@@ -54,8 +55,8 @@ class ChooseNext {
         // controllo che la car possa arrivare in tempo al Ride AND
         // controllo che la macchina arrivi antro s unita dalla partenza
         return (time + distCarRide + distPercorso <= maxTime &&
-            r.f <= time + distCarRide + distPercorso &&
-            r.s >= time + distCarRide + slack);
+            time + distCarRide + distPercorso <= r.f &&
+            time + distCarRide + slack >= r.s);
 
     }
 
