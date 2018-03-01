8cac2f93ba82636d3e93ab2c2b79fadff3b7e563
Manu@ares-xps15 <manuel.turetta94@gmail.com>
2018-03-01 21:10:49 +0100

    Inizio del main

--- cars/src/main/java/com/hascode/cars/Main.java
+++ cars/src/main/java/com/hascode/cars/Main.java
@@ -2,12 +2,28 @@ package com.hascode.cars;
 
 import com.hashcode.cars.OutputWriter;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args){
+    private static final String ES1 = "a_example.in";
+    private static final String ES2 = "b_should_be_easy.in";
+    private static final String ES3 = "c_no_hurry.in";
+    private static final String ES4 = "d_metropolis.in";
+    private static final String ES5 = "e_high_bonus.in";
+
+    public static void main(String[] args) throws URISyntaxException {
+        InputStreamReader resource = new InputStreamReader(Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream(ES1));
+        BufferedReader bufferedInputStream = new BufferedReader(resource);
+        Parser parser = new Parser(bufferedInputStream.lines());
+        Map<String, Integer> data = parser.getData();
+        data.get("t");
     }
 
 }
